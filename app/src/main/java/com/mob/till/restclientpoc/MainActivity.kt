a25ab8b996e0ff2b6b497ad844dcc8767778978e
Togay Han Kaya <tkaya@mobiquityinc.com>
2022-07-28 12:50:37 +0200

    Make network request

--- app/src/main/java/com/mob/till/restclientpoc/MainActivity.kt
+++ app/src/main/java/com/mob/till/restclientpoc/MainActivity.kt
@@ -2,10 +2,14 @@ package com.mob.till.restclientpoc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mob.till.librarypoc.NetworkLibrary
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val networkLib = NetworkLibrary()
+        networkLib.makeRequest(this)
     }
 }
